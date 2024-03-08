c01d035f3ad595a8cf8a17c49f99b166d7e26fa7
Kevin Ellis <kevin.ellis@morrisonsplc.co.uk>
2024-03-08 16:50:20 +0000

    Add post for Labyrinth. Hold back Jekyll container version for compatibility

--- run-jekyll-docker.sh
+++ run-jekyll-docker.sh
@@ -1,1 +1,1 @@
-docker run -t -i --rm --name jekyll -e POLLING=true -v "$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll:pages jekyll serve
+docker run -t -i --rm --name jekyll -e POLLING=true -v "$PWD:/srv/jekyll" -p 4000:4000 jekyll/jekyll:4.2.0 jekyll serve
